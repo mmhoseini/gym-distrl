f0f2b1912dd5ddb50058dce5aebe7ee45c7e12c4
Hieu Nguyen <hieu.trung.nguyen@emt.inrs.ca>
2019-04-09 12:49:57 -0600

    Update distrl_env.py

--- gym_distrl/envs/distrl_env.py
+++ gym_distrl/envs/distrl_env.py
@@ -3,15 +3,22 @@ from gym import error, spaces, utils
 from gym.utils import seeding
 
 class DistrlEnv(gym.Env):
-  metadata = {'render.modes': ['human']}
-
-  def __init__(self):
-    ...
-  def step(self, action):
-    ...
-  def reset(self):
-    ...
-  def render(self, mode='human'):
-    ...
-  def close(self):
-    ...
+	metadata = {'render.modes': ['human']}
+	
+	def __init__(self):
+	return self
+	
+	def step(self, action):
+	
+	return self
+	
+	def reset(self):
+	
+	return self
+	
+	def render(self, mode='human'):
+	
+	return self
+	
+	def close(self):
+	return  self
